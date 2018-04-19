5e2f57a8d25774cca7bcc7b1990d50f41b722870
Richard Ben <frichardben@yahoo.com.br>
2018-04-19 14:52:06 -0300

    first project

---
+++ lesson 1/Caesar_Cipher.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(offset, string)
+    encrypt_str = ""
+    alph="abcdefghijklmnopqrstuvwxyz"
+
+    string.split("").each {|c|
+        if c != ""
+            c_index = alph.index(c)
+            new_pos = c_index + offset
+            new_alph_index = new_pos % 26
+            encrypt_str = encrypt_str + alph[new_alph_index]
+        else
+            encrypt_str = encrypt_str + ""
+        end
+    }
+    return encrypt_str
+end
